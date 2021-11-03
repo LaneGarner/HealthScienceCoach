1f423e90a61899e738d0412b7272705840a1c0d5
Lane Garner <b.lane.garner@gmail.com>
2021-11-03 18:33:56 -0500

    added assets page and logo images

--- src/Router.js
+++ src/Router.js
@@ -1,9 +1,10 @@
 import { About } from "./pages/About";
-import { Home } from "./pages/Home";
+import { Assets } from "./pages/Assets";
 import { Contact } from "./pages/Contact";
 import { Episodes } from "./pages/Episodes";
-// import { Sponsors } from "./pages/Sponsors";
+import { Home } from "./pages/Home";
 import { Podcast } from "./components/Podcast";
+// import { Sponsors } from "./pages/Sponsors";
 
 import { Switch, Route } from "react-router";
 
@@ -15,6 +16,7 @@ export const Router = () => {
       <Route path="/episodes" component={Episodes} />
       <Route exact path="/" component={Home} />
       <Route exact path="/podcast/:episode" component={Podcast} />
+      <Route path="/assets" component={Assets} />
       {/* <Route path="/sponsors" component={Sponsors} /> */}
     </Switch>
   );
---
+++ src/pages/Assets.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Assets = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Health Science Coach Assets</h1>
+      <a href="https://healthsciencecoach.com/img/hsc-logo-black-background.jpg">hsc-logo-black-background.jpg</a>
+    </div>
+  );
+};
