ef92f569ca87c81af3dd81fffec71fa23d53362b
Lane Garner <b.lane.garner@gmail.com>
2022-01-28 22:23:21 -0600

    initial ga setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from 'react-ga';
 
 import { Router } from "./Router";
 import { Header } from "./components/Header.jsx";
 import { ScrollToTop } from "./components/ScrollToTop";
 
 export const App = () => {
+  ReactGA.initialize(process.env.REACT_APP_GA_TRACKING_ID);
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <BrowserRouter>
       <ScrollToTop />
