df4e562993341b685ce8335fcbe22312dd18d6de
Lane Garner <b.lane.garner@gmail.com>
2022-03-25 18:33:32 -0500

    add about me image to about me page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,12 @@ import "./About.scss";
 export const About = () => {
   return (
     <div className="page-container about-page">
-      {/* <img src="https://healthsciencecoach.com/img/hirepaths_blog-images_health-career-podcast.png" alt="Drew Garner hosting health science coach podcast with yellow geometric design in background" /> */}
+      <img 
+        src="https://healthsciencecoach.com/img/hirepaths_blog-images_health-career-podcast.png" 
+        alt="Drew Garner hosting health science coach podcast with yellow geometric design in background" 
+        style={{width: "80%"}}
+      />
       <p className="hey-coach">"Hey Coach Garner, how do I become a nurse practitioner?"</p>
-      <Logo width="200px" />
       <p className="about-description">
         I was asked this question in 2017 by a high school senior five weeks before graduation. This led to developing the Career and Technical Education Health Science Pathway at
         Turner High School and a partnership with Kansas City Kansas Community College certifying students as Nursing Assistants and Medical Aids when graduating from high school.
