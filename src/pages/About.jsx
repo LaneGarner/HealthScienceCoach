6d1dceab93104b1ebb6ca50ea8f0571640a0210f
Lane Garner <b.lane.garner@gmail.com>
2022-03-25 18:15:06 -0500

    add about me image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,12 +5,14 @@ import "./About.scss";
 export const About = () => {
   return (
     <div className="page-container about-page">
-      <Logo width="200px" />
+      {/* <img src="https://healthsciencecoach.com/img/hirepaths_blog-images_health-career-podcast.png" alt="Drew Garner hosting health science coach podcast with yellow geometric design in background" /> */}
       <p className="hey-coach">"Hey Coach Garner, how do I become a nurse practitioner?"</p>
+      <Logo width="200px" />
       <p className="about-description">
         I was asked this question in 2017 by a high school senior five weeks before graduation. This led to developing the Career and Technical Education Health Science Pathway at
         Turner High School and a partnership with Kansas City Kansas Community College certifying students as Nursing Assistants and Medical Aids when graduating from high school.
       </p>
+      {/* <iframe src="https://outlook.office365.com/owa/calendar/HealthScienceCoachPodcast@turnerusd202.org/bookings/" width="100%" height="100%" scrolling="yes" style={{border:0}}></iframe> */}
     </div>
   );
 };
